c228a0e49de08e4e9e9ad70b9b42b7acd038d265
Bryan Duarte <stfrisephone@gmail.com>
2021-08-14 18:45:18 -0500

    Added index file for sample app (not yet tested)

---
+++ api/src/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    use Small\Core\Application;
+    use Small\Interfaces\RequestInterface;
+    use Small\Interfaces\ResponseInterface;
+
+    $app = new Application();
+
+    $app->get('/', function (RequestInterface $request, ResponseInterface $response) {
+        $response->getBody()->write('Index');
+        return $response;
+    });
+
+    $app->get('/contact', function (RequestInterface $request, ResponseInterface $response) {
+        $response->getBody()->write('Contact');
+        return $response;
+    });
+
+    $app->run();
