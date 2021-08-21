03234c51919560de49293661d1eddaa5fe088451
Bryan Duarte <stfrisephone@gmail.com>
2021-08-21 13:36:02 -0500

    Added StreamFactory

---
+++ api/src/Small/Core/StreamFactory.php
@@ -0,0 +1,31 @@
+<?php
+    namespace Small\Core;
+
+    use Small\Interfaces\StreamInterface;
+
+    class StreamFactory
+    {
+        public function create(string $content = '') : StreamInterface
+        {
+            $resource = fopen('php://temp', 'rw+');
+
+            if (!is_resource($resource)) {
+                // TODO: throw exception
+            }
+
+            fwrite($resource, $content);
+            rewind($resource);
+
+            return $this->createFromResource($resource);
+        }
+
+        public function createFromResource($resource) : StreamInterface
+        {
+            if (!is_resource($resource)) {
+                // TODO: Throw exception
+            }
+
+            return new Stream($resource);
+        }
+    }
+    
