786041aa63c1a0e4c24c2852af260aae64dce627
Bryan Duarte <stfrisephone@gmail.com>
2021-08-21 13:57:13 -0500

    Modified ResponseEmitter to emit body according to the new StreamInterface

--- api/src/Small/Core/ResponseEmitter.php
+++ api/src/Small/Core/ResponseEmitter.php
@@ -7,6 +7,13 @@
 
     class ResponseEmitter
     {
+        private $responseChunkSize;
+
+        public function __construct(int $responseChunkSize = 4096)
+        {
+            $this->responseChunkSize = $responseChunkSize;
+        }
+
         public function emit(ResponseInterface $response)
         {
             $this->emitStatusLine($response);
@@ -24,9 +31,36 @@
 
         private function emitBody(ResponseInterface $response)
         {
-            // TEMPORAL
-            $length = $response->getBody()->getSize();
-            echo $response->getBody()->read($length);
+            $body = $response->getBody();
+            if ($body->isSeekable()) {
+                $body->rewind();
+            }
+
+            $amountToRead = (int) $response->getHeaderLine('Content-Length');
+            if (!$amountToRead) {
+                $amountToRead = $body->getSize();
+            }
+
+            if ($amountToRead) {
+                while ($amountToRead > 0 && !$body->eof()) {
+                    $length = min($this->responseChunkSize, $amountToRead);
+                    $data = $body->read($length);
+                    echo $data;
+
+                    $amountToRead -= strlen($data);
+
+                    if (connection_status() !== CONNECTION_NORMAL) {
+                        break;
+                    }
+                }
+            } else {
+                while (!$body->eof()) {
+                    echo $body->read($this->responseChunkSize);
+                    if (connection_status() !== CONNECTION_NORMAL) {
+                        break;
+                    }
+                }
+            }
         }
 
         private function emitStatusLine(ResponseInterface $response)
