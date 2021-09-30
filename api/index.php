a9eb15695b311eddc35ab68b39ade122d3ee1dcd
Bryan Duarte <stfrisephone@gmail.com>
2021-09-30 18:34:17 -0500

    Tested User registration using Api

--- api/index.php
+++ api/index.php
@@ -2,44 +2,18 @@
     require_once __DIR__ . '/vendor/autoload.php';
 
     use Small\Core\Application;
-    use Small\Interfaces\RequestInterface;
-    use Small\Interfaces\ResponseInterface;
 
     use Small\Middleware\BodyParserMiddleware;
+
+    use Didactinaut\Controllers\UsersController;
+    use Didactinaut\Factories\Database\MySQLDatabaseFactory;
+
+    $databaseFactory = new MySQLDatabaseFactory('localhost', 'didactinaut_dev');
 
     $app = new Application();
 
     $app->addMiddleware(new BodyParserMiddleware());
 
-    $app->get('/api', function (RequestInterface $request, ResponseInterface $response, $args) {
-        
-        $test = $request->getAttribute("test", "Default");
-        $response->getBody()->write("$test Index: ");
-
-        $queryParams = $request->getQueryParams();
-        $username = $queryParams['username'] ?? 'Invitado';
-        $edad = $queryParams['age'] ?? '?';
-
-        $response->getBody()->write(" Bienvenido $username tienes $edad años");
-
-        return $response;
-    });
-
-    $app->post('/api', function (RequestInterface $request, ResponseInterface $response, $args) {
-        $parsedBody = $request->getParsedBody();
-        $username = $parsedBody['username'] ?? 'Invitado';
-        $edad = $parsedBody['age'] ?? '?';
-
-        $response->getBody()->write(" Bienvenido $username tienes $edad años");
-
-        return $response;
-    });
-
-    $app->get('/api/contact/$id/$username', function (RequestInterface $request, ResponseInterface $response, $args) {
-        $id = $request->getAttribute('id');
-        $username = $request->getAttribute('username');
-        $response->getBody()->write("Hello! I'm $username with id $id");
-        return $response;
-    });
+    $app->post('/api/users/', [new UsersController($databaseFactory), 'postUser']);
 
     $app->run();
