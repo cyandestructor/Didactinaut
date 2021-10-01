27e13f9c4d09fb0a88d2ca8f936d9dadcd575f87
Bryan Duarte <stfrisephone@gmail.com>
2021-10-01 12:17:07 -0500

    Fixed index.php indentation

--- api/index.php
+++ api/index.php
@@ -1,32 +1,32 @@
 <?php
-    require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-    use Small\Core\Application;
+use Small\Core\Application;
 
-    use Small\Middleware\BodyParserMiddleware;
+use Small\Middleware\BodyParserMiddleware;
 
-    use Didactinaut\Controllers\UsersController;
-    use Didactinaut\Controllers\ImagesController;
-    use Didactinaut\Factories\Database\MySQLDatabaseFactory;
+use Didactinaut\Controllers\UsersController;
+use Didactinaut\Controllers\ImagesController;
+use Didactinaut\Factories\Database\MySQLDatabaseFactory;
 
-    $databaseFactory = new MySQLDatabaseFactory('localhost', 'didactinaut_dev');
+$databaseFactory = new MySQLDatabaseFactory('localhost', 'didactinaut_dev');
 
-    $app = new Application();
+$app = new Application();
 
-    $app->addMiddleware(new BodyParserMiddleware());
+$app->addMiddleware(new BodyParserMiddleware());
 
-    // Users
-    {
-        $app->get('/api/users/', [new UsersController($databaseFactory), 'checkUserExists']);
-        $app->get('/api/users/$id', [new UsersController($databaseFactory), 'getUnique']);
-        $app->post('/api/users/', [new UsersController($databaseFactory), 'postUser']);
-        $app->put('/api/users/$id', [new UsersController($databaseFactory), 'putUser']);
-        $app->put('/api/users/$id/image', [new UsersController($databaseFactory), 'putUserImage']);
-    }
+// Users
+{
+    $app->get('/api/users/', [new UsersController($databaseFactory), 'checkUserExists']);
+    $app->get('/api/users/$id', [new UsersController($databaseFactory), 'getUnique']);
+    $app->post('/api/users/', [new UsersController($databaseFactory), 'postUser']);
+    $app->put('/api/users/$id', [new UsersController($databaseFactory), 'putUser']);
+    $app->put('/api/users/$id/image', [new UsersController($databaseFactory), 'putUserImage']);
+}
 
-    // Images
-    {
-        $app->get('/api/images/$id', [new ImagesController($databaseFactory), 'getUnique']);
-    }
+// Images
+{
+    $app->get('/api/images/$id', [new ImagesController($databaseFactory), 'getUnique']);
+}
 
-    $app->run();
+$app->run();
